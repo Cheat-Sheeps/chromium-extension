9d8ae22a844a31d4e306bd73e8865635cb32377f
Thurinum <m.gagnon941@gmail.com>
2023-10-14 12:46:07 -0400

    feat: Setup Chrome extension and webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path")
+const CopyPlugin = require("copy-webpack-plugin")
+
+module.exports = {
+    mode: "production",
+    entry: {
+        main: path.resolve(__dirname, "..", "src", "scripts", "main.ts"),
+    },
+    output: {
+        path: path.join(__dirname, "../dist"),
+        filename: "[name].js"
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {from: "./src/popup.html", to: ".", context: "."},
+                {from: "./src/assets", to: ".", context: "."},
+                {from: ".", to: ".", context: "public"}
+            ]
+        })
+    ]
+}
