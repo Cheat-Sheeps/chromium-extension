0c331a9e081ae927a199d4f8e801fd3e1c6dd947
Theodore L'Heureux <theodorelheureux@gmail.com>
2023-10-15 01:10:39 -0400

    change logo and enable watch mode

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,34 +2,35 @@ const path = require("path")
 const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
-    mode: "production",
-    entry: {
-        main: path.resolve(__dirname, "..", "src", "scripts", "main.ts"),
-    },
-    output: {
-        path: path.join(__dirname, "../dist"),
-        filename: "[name].js"
-    },
-    resolve: {
-        extensions: [".ts", ".js"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader",
-                exclude: /node_modules/
-            }
-        ]
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                {from: "./src/popup.html", to: ".", context: "."},
-                {from: "./src/popup.css", to: ".", context: "."},
-                {from: "./src/assets", to: ".", context: "."},
-                {from: ".", to: ".", context: "public"}
-            ]
-        })
-    ]
-}
+	mode: "production",
+	watch: true,
+	entry: {
+		main: path.resolve(__dirname, "..", "src", "scripts", "main.ts"),
+	},
+	output: {
+		path: path.join(__dirname, "../dist"),
+		filename: "[name].js",
+	},
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [
+				{ from: "./src/popup.html", to: ".", context: "." },
+				{ from: "./src/popup.css", to: ".", context: "." },
+				{ from: "./src/assets", to: ".", context: "." },
+				{ from: ".", to: ".", context: "public" },
+			],
+		}),
+	],
+};
